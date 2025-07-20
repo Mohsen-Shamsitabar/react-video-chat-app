d679f0766de09c9b9b1b2ab55baa4ca5c46876c8
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-07-20 11:21:00 +0330

    add placeholder

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -1,6 +1,18 @@
 import ThemeToggler from "./theme-toggler.tsx";
 
 const Header = () => {
+  // React.useEffect(() => {
+  //   const socket = io(NETWORK.SERVER_URL);
+
+  //   socket.on("connect", () => {
+  //     console.log(`SOCKET:${socket.id} connected!`);
+  //   });
+
+  //   return () => {
+  //     socket.removeListener("connect");
+  //   };
+  // });
+
   return (
     <header className="main-container border-b-2 py-1">
       <div className="container">
