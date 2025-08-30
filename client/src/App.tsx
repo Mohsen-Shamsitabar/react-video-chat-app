6f29696ad74b4996775601dbad1ea47209519852
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-08-30 14:33:12 +0330

    move `NotFoundPage`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import "normalize.css";
 import { Route, Routes } from "react-router";
-import { Header } from "./components/common/index.ts";
+import { Header, NotFoundPage } from "./components/common/index.ts";
 import AuthLayout from "./layouts/auth-layout.tsx";
 import { CHATROOM_PARAM_NAME, PAGE_ROUTES } from "./lib/constants.ts";
 import ChatroomPage from "./pages/chatroom/chatroom-page.tsx";
 import DashboardPage from "./pages/dashboard/dashboard-page.tsx";
 import LoginPage from "./pages/login-page.tsx";
-import NotFoundPage from "./pages/not-found-page.tsx";
 
 const App = () => {
   return (
